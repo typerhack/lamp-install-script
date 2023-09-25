1ea2bd3b5c01217a3aee96b73fd1e61b66d05a63
Daniel Dolatabadi - PC <hybevo@gmail.com>
2023-09-25 15:15:33 +0330

    v0.2 fixed some bugs

--- lamp-install.sh
+++ lamp-install.sh
@@ -183,7 +183,7 @@ add_user_premission () {
 # This Function installs git
 git_install () {
     echo Installing git
-    sudo apt install git-all
+    sudo apt install git-all -y
     echo "Done!"
 }
 
@@ -197,14 +197,14 @@ run_vscode () {
 # This function installs vscode
 install_vscode () {
     echo Installing VSCode....
-    sudo apt-get install wget gpg
+    sudo apt-get install wget gpg -y
     wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
     sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
     sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
     rm -f packages.microsoft.gpg
-    sudo apt install apt-transport-https
+    sudo apt install apt-transport-https -y
     sudo apt update
-    sudo apt install code
+    sudo apt install code -y
     echo "Done!"
 }
 
